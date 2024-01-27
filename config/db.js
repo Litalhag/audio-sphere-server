63a90c4d259aafd5652d2e44c3085ace919aaa21
Litalhag <litalhag@gmail.com>
2024-01-27 20:42:42 +0200

    db

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {})
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+}
+
+export default connectDB
+
+// useNewUrlParser: true,
+// useCreateIndex: true,
+// useFindAndModify: false,
+// useUndefinedTopology: true,
